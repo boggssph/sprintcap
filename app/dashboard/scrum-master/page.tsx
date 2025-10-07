af8d09e2c90ef1d3dd4825144b8186b1054e4f42
boggssph <ftseguerra@gmail.com>
2025-10-06 21:22:35 -0400

    style(layout): left-align dashboard main container (remove mx-auto)

--- app/dashboard/scrum-master/page.tsx
+++ app/dashboard/scrum-master/page.tsx
@@ -512,7 +512,7 @@ export default function ScrumMasterDashboard() {
           <SidebarInset>
             {/* Header */}
             <ScrumMasterHeader />
-            <main className="w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6 flex-1 flex flex-col">
+            <main className="w-full max-w-7xl px-4 sm:px-6 lg:px-8 py-6 flex-1 flex flex-col">
               {/* Modern hero / KPI area */}
               {view === 'overview' && (
                 <section className="mb-6">
